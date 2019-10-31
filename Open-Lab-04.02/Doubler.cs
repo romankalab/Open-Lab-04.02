404fb74dafd9d5afb9f0dac63f6176d8b1999eea
romankalab <roan.kalab@mail.com>
2019-10-31 18:34:16 +0100

    Initial

--- Open-Lab-04.02/Doubler.cs
+++ Open-Lab-04.02/Doubler.cs
@@ -6,7 +6,10 @@ namespace Open_Lab_04._02
     {
         public string DoubleChar(string original)
         {
-            throw new NotImplementedException();
+            string result = "";
+            for (var x = 0; x < original.Length; x++)
+                result = result + original[x] + original[x];
+            return result;
         }
     }
 }
